d184aaa792f8d08d9d3b9c7d307407d73d45fdff
Brett Andrews <brettandrews3@gmail.com>
2020-06-29 09:24:30 -0500

    Day 1 work done. Launching Day 2

---
+++ TestAPI/Models/ToDoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore; //Unnecessary using statement?
+
+namespace TestAPI.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+        //Next, the DbContext needs to be registered with the dependency
+        //injection (DI) containe. Container provides service to controllers.
+    }
+}
---
+++ TestAPI/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TestAPI.Models
+{
+    public class ToDoItem
+    {
+        public long Id { get; set; } //setting up the primary key for the database
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
